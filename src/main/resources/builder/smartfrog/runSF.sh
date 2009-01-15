063141ac0d0df1aa66ba43cf925de91b7b61fed9
Radoslav Husar <radosoft@gmail.com>
2009-01-15 13:51:28 +0000

    updated plugin; jira https://jira.jboss.org/jira/browse/JBQA-1895 fixes done: make raw log correctly viewable; add smartfrog user homes 1-4

--- src/main/resources/builder/smartfrog/runSF.sh
+++ src/main/resources/builder/smartfrog/runSF.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -x $1 "export JAVA_HOME=$JAVA_HOME; export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin$PATH; export SFHOME=$2; export SFUSERHOME=$3; export SFUSERHOME1=$4; cd $5; export JVMARGS='$6'; export SFDEFAULTINI=-Dorg.smartfrog.iniFile=$7; $2/bin/sfDaemon"
+ssh -x $1 "export JAVA_HOME=$JAVA_HOME; export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin$PATH; export SFHOME=$2; export SFUSERHOME=$3; export SFUSERHOME1=$4; export SFUSERHOME2=$5; export SFUSERHOME3=$6; export SFUSERHOME4=$7; cd $8; export JVMARGS='$9'; export SFDEFAULTINI=-Dorg.smartfrog.iniFile=${10}; $2/bin/sfDaemon -d"
