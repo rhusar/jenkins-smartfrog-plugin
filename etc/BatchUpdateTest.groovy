68d1f072021f974be76962a2b55f55b3a4dc68ee
Radoslav Husar <radosoft@gmail.com>
2010-08-04 11:16:02 +0000

    update to the groovy script

--- etc/BatchUpdateTest.groovy
+++ etc/BatchUpdateTest.groovy
@@ -24,12 +24,27 @@ import builder.smartfrog.*
 import hudson.tasks.*
 import java.util.regex.Pattern
 
+/*
 def projects = Hudson.instance.getAllItems(Project.class)
 
-def jobs = ["radotest"]
+for(project in projects) {
+  if ("${project.name}".contains("eap-5x-failover-http-random")) {
+   //println "\n* ${project.name}"
+      print "\"${project.name}\","
+   }
+}
+ */
+
+//def jobs = ["eap-5x-failover-http-jvmkill-buddy-async","eap-5x-failover-http-jvmkill-buddy-sync","eap-5x-failover-http-jvmkill-total-async","eap-5x-failover-http-jvmkill-total-sync","eap-5x-failover-http-random-jvmkill-buddy-async","eap-5x-failover-http-random-jvmkill-buddy-sync","eap-5x-failover-http-random-jvmkill-total-async","eap-5x-failover-http-random-jvmkill-total-sync","eap-5x-failover-http-random-shutdown-buddy-async","eap-5x-failover-http-random-shutdown-buddy-sync","eap-5x-failover-http-random-shutdown-total-async","eap-5x-failover-http-random-shutdown-total-sync","eap-5x-failover-http-random-undeploy-buddy-async","eap-5x-failover-http-random-undeploy-buddy-sync","eap-5x-failover-http-random-undeploy-total-async","eap-5x-failover-http-random-undeploy-total-sync","eap-5x-failover-http-shutdown-buddy-async","eap-5x-failover-http-shutdown-buddy-sync","eap-5x-failover-http-shutdown-total-async","eap-5x-failover-http-shutdown-total-sync","eap-5x-failover-http-undeploy-buddy-async","eap-5x-failover-http-undeploy-buddy-sync","eap-5x-failover-http-undeploy-total-async","eap-5x-failover-http-undeploy-total-sync"]
+def jobs = ["eap-5x-failover-http-random-jvmkill-buddy-async","eap-5x-failover-http-random-jvmkill-buddy-sync","eap-5x-failover-http-random-jvmkill-total-async","eap-5x-failover-http-random-jvmkill-total-sync","eap-5x-failover-http-random-shutdown-buddy-async","eap-5x-failover-http-random-shutdown-buddy-sync","eap-5x-failover-http-random-shutdown-total-async","eap-5x-failover-http-random-shutdown-total-sync","eap-5x-failover-http-random-undeploy-buddy-async","eap-5x-failover-http-random-undeploy-buddy-sync","eap-5x-failover-http-random-undeploy-total-async","eap-5x-failover-http-random-undeploy-total-sync"]
 
 for (job in jobs){
    item = Hudson.instance.getItem(job)
+
+   println "\n* processing ${item.name}"
+
+   // TORENAME item.renameTo("radotest2")
+   
    builders = item.buildersList
       
    // SCM
@@ -47,21 +62,21 @@ for (job in jobs){
 
    // SF BUILDER
    sfBuilder = builders.get(SmartFrogBuilder.DESCRIPTOR)
-   sfBuilder.@sfUserHome = "maven/eap-51/target/dependency"
+   sfBuilder.@sfUserHome = "etc/maven/eap-51/target/dependency"
    sfBuilder.jvmArgs  = "-server -Xmx4g -XX:+UseLargePages"
-   //println sfBuilder.scriptContent.readLines().get(0)
-   sfBuilder.@scriptContent = sfBuilder.scriptContent.replaceAll("org/jboss/smartfrog/test/ejb3/template-failover-ejb3-order.sf", "org/jboss/smartfrog/eap/failover/template-http-order.sf")
-
-   /*def newcont =
-   for (String line in sfBuilder.scriptContent.readLines()) {
-
-   }*/
-
+   //sfBuilder.@scriptContent = sfBuilder.scriptContent.replaceAll("org/jboss/smartfrog/test/ejb3/template-failover-ejb3-order.sf", "org/jboss/smartfrog/eap/failover/template-ejb3-order.sf")
+   sfBuilder.@scriptContent = sfBuilder.scriptContent.replaceAll("org/jboss/smartfrog/test/failover/template-http-order.sf", "org/jboss/smartfrog/eap/failover/template-http-order.sf")
    sfBuilder.hosts = "perf09 perf01 perf02 perf03 perf04 perf05"
 
    // update maven plugin
    mavenb = builders.get(Maven.DESCRIPTOR)
    mavenb.@pom = "maven/eap-51/hudson-failover.xml"
+
+   // make the workspace delete after the run
+   list = item.getPublishersList()
+   if(!list.contains(hudson.plugins.ws_cleanup.WsCleanup.DESCRIPTOR)){
+      list.add(new hudson.plugins.ws_cleanup.WsCleanup())
+   }
 
 
    // persist to file (helps on hudson restart ;)
