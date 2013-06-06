48a19e2ca03570b8afb7ff1535d8ae20e8a04b7e
Vojtech Juranek <vjuranek@redhat.com>
2013-06-06 16:33:58 +0200

    Enable build parameter expansion for file script source

--- src/main/java/builder/smartfrog/FileScriptSource.java
+++ src/main/java/builder/smartfrog/FileScriptSource.java
@@ -1,9 +1,15 @@
 package builder.smartfrog;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractBuild;
 import hudson.model.Hudson;
+import hudson.model.ParametersAction;
 
 /**
  * 
@@ -13,6 +19,7 @@ import hudson.model.Hudson;
 public class FileScriptSource extends ScriptSource {
 
     private String scriptPath;
+    private File tmpScriptFile;
     
     @DataBoundConstructor
     public FileScriptSource(String scriptName, String scriptPath){
@@ -25,7 +32,17 @@ public class FileScriptSource extends ScriptSource {
     }
     
     public String getDefaultScriptPath() {
+        if(tmpScriptFile != null)
+            return tmpScriptFile.getPath();
         return scriptPath;
+    }
+    
+    @Override
+    public void createScriptFile(AbstractBuild<?,?> build) throws InterruptedException, IOException {
+        FilePath fp = new FilePath(build.getWorkspace(), getScriptPath());
+        String scriptContent = fp.readToString(); // TODO not very safe, if e.g. some malicious user provide path to huge file
+        FilePath path = createDefaultScriptFile(scriptContent, build);
+        tmpScriptFile = new File(path.getRemote());
     }
     
     public DescriptorImpl getDescriptor(){
