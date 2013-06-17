c1c97757efdf5878941bd55330ab06ba4fda49e9
Vojtech Juranek <vjuranek@redhat.com>
2013-06-18 00:49:41 +0200

    Fix failing test

--- src/test/java/builder/smartfrog/SmartFrogActionTest.java
+++ src/test/java/builder/smartfrog/SmartFrogActionTest.java
@@ -8,7 +8,7 @@ public class SmartFrogActionTest {
     @Test
     public void encodeIPv6Hostname() {
         SmartFrogAction sfa = new SmartFrogAction(null, "::1");
-        assertEquals("console-%3A%3A1-0", sfa.getUrlName());
+        assertEquals("console-%3A%3A1", sfa.getUrlName());
     }
 
 }
